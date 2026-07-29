baabdb84243b99368d996a85081888c5c6353e67
chell <chell.mailbox@gmail.com>
2026-07-29 17:54:50 +0700

    feat: Base logic: URL receiving added.

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatalf("golog: No URL given\n")
+	}
+
+	url := args[1] // URL of web-site to check.
 
 }
